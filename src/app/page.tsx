8db8087d42e112a3ef0c2e4a879cec26c7a16ca4
poiuyt183 <khuyen.dev183@gmail.com>
2026-04-29 15:20:25 +0700

    feat: trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import prisma from '../../lib/prisma'
+import { caller } from '@/trpc/server'
 
 const Page = async () => {
-  const users = await prisma.user.findMany()
+  const users = await caller.getUsers()
   return (
     <div className='flex justify-center items-center h-screen'>
       {JSON.stringify(users)}
